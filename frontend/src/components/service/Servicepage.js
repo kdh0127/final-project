97641c4aee2bf151d10cad94db2842c24602f76f
kimdoheon <joeunman11@naver.com>
2024-12-04 16:57:25 +0900

    서비스페이지 업데이트
    서비스페이지 상단페이지 작업완료

--- frontend/src/components/service/Servicepage.js
+++ frontend/src/components/service/Servicepage.js
@@ -11,26 +11,34 @@ function Service(){
         {   
             header: "디지털 스마트 부산 아카데미",
             subheader: "9월 2일 ~ 12월 20일 | B04(C)-0107",
-            title: "신규 양봉업자를 위한 꿀벌 질병 진단 및 치료 편의 제공 서비스",
+            title: "신규 양봉업자를 위한 질병관리 서비스",
             article: "구성원 | 배규태 고지수 김도헌 서지민 정동윤",
             more: "더 알아보기",
-            imageUrl: "servicelogo.png", 
+            imageUrl: "servicelogo1.png", 
           },
           {
-            header: "Screen 2 Header",
-            subheader: "Screen 2 subHeader",
-            title: "Discover Screen 2",
-            article: "This is the second screen content.",
-            more: "Discover",
-            imageUrl: "/images/image2.jpg", 
+            header: "Beecareful의 첫 번째 서비스",
+            subheader: "간편하게 질병명을 알아보는 서비스",
+            title: "질병 이미지분류",
+            article: "직접 찍은 사진으로 바로 질병을 확인할 수 있습니다. 정확하고 빠른 이미지 진단으로 대처하세요.",
+            more: "더 알아보기",
+            imageUrl: "result_bee.jpg", 
           },
           {
-            header: "Screen 3 Header",
-            subheader: "Screen 3 subHeader",
-            title: "Explore Screen 3",
-            article: "This is the third screen content.",
-            more: "Explore",
-            imageUrl: "/images/image3.jpg", 
+            header: "Beecareful의 두 번째 서비스",
+            subheader: "질병의 발생을 감시해주는 서비스",
+            title: "질병 발생 감시",
+            article: "벌통에 설치된 카메라를 통해  질병이 발생시 문자메세지를 발송해줍니다. 이 서비스로 초기에 대응하세요.",
+            more: "더 알아보기",
+            imageUrl: "result2.jpg", 
+          },
+          {
+            header: "Beecareful의 세 번째 서비스",
+            subheader: "꿀벌에 관한 다양한 정보를 제공해주는 서비스",
+            title: "꿀벌 특화 Chatbot",
+            article: "꿀벌질병에 대한 예방조치 부터 치료법, 주변 환경조성 등 다양한 부분을 대답해드립니다.",
+            more: "더 알아보기",
+            imageUrl: "beechat.png", 
           },
         ];
 
@@ -52,8 +60,12 @@ function Service(){
                     article={screens[currentScreen].article}
                     more={screens[currentScreen].more}
                     imageUrl={screens[currentScreen].imageUrl}
+                    screens={screens}
+                    currentScreen={currentScreen}
+                    setCurrentScreen={setCurrentScreen}
                     />
                 </div>
+                
                 <div className={style.service2}>
                     
                 </div>
