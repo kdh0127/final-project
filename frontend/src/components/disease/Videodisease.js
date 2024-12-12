6527a0efea397cbd8656b8bbe79fc9091bd52f1b
kimdoheon <joeunman11@naver.com>
2024-12-12 10:21:37 +0900

    감시시스템 초안 완성
    어떤 요소가 들어갈지 디테일 수정 필요

--- frontend/src/components/disease/Videodisease.js
+++ frontend/src/components/disease/Videodisease.js
@@ -15,47 +15,62 @@ function Videodisease(){
                 </div>
                 <div className={style.body}>
                     <div className={style.leftbody}>
-                        <div className={style.upbox}>
-                            <div className={style.left_topbox1}>
-                                <span className={style.box_span}>
-                                    10분 전
-                                </span>
+                        <div className={style.left_container1}>
+                            <div className={style.upbox}>
+                                <div className={style.left_topbox1}>
+                                    <span className={style.box_span}>
+                                        10분 전
+                                    </span>
+                                </div>
+                                <div className={style.left_topbox2}>
+                                    <span className={style.box_span}>
+                                        30분 전
+                                    </span>
+                                </div>
+                                <div className={style.left_topbox3}>
+                                    <span className={style.box_span}>
+                                        1시간 전
+                                    </span>
+                                </div>
                             </div>
-                            <div className={style.left_topbox2}>
-                                <span className={style.box_span}>
-                                    30분 전
-                                </span>
-                            </div>
-                            <div className={style.left_topbox3}>
-                                <span className={style.box_span}>
-                                    1시간 전
-                                </span>
+                            <div className={style.downbox}>
+                                <div className={style.left_topbox4}>
+                                    <span className={style.box_span}>
+                                        6시간 전
+                                    </span>
+                                </div>
+                                <div className={style.left_topbox5}>
+                                    <span className={style.box_span}>
+                                        12시간 전
+                                    </span>
+                                </div>
+                                <div className={style.left_topbox6}>
+                                    <span className={style.box_span}>
+                                        1일 전
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                        <div className={style.downbox}>
-                            <div className={style.left_topbox4}>
-                                <span className={style.box_span}>
-                                    6시간 전
+                        <div className={style.left_container2}>
+                            <div className={style.example_up}>
+                                <span className={style.example_title}>
+                                    문자 예시
                                 </span>
                             </div>
-                            <div className={style.left_topbox5}>
-                                <span className={style.box_span}>
-                                    12시간 전
+                            <div className={style.example_down}>
+                                <span className={style.example_text}>
+                                    <br/>[Web발신] <br/><br/> □ 15:06 부산광역시 기장군 기장읍 기장대로 313 <br/><br/> □ [3번 벌통] 응애병 감지
                                 </span>
                             </div>
-                            <div className={style.left_topbox6}>
-                                <span className={style.box_span}>
-                                    1일 전
-                                </span>
-                            </div>
-                            
-                        </div>
-                        <div className={style.left_body}>
-
                         </div>
                     </div>
                     <div className={style.rightbody}>
-                        
+                        <div className={style.video_text}>
+                            <span className={style.video_span}>영상자리</span>
+                        </div>
+                        <div className={style.videobox}>
+                            test
+                        </div>
                     </div>
                 </div>
             </div>
