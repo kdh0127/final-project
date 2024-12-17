fd6dc4ad2f68487a72246fb76e1a5cf619d83123
kimdoheon <joeunman11@naver.com>
2024-12-17 15:32:14 +0900

    내정보페이지 coming soon으로 수정
    배포하기 위한 netlify.toml추가

--- frontend/src/components/account/Myaccount.js
+++ frontend/src/components/account/Myaccount.js
@@ -7,7 +7,16 @@ function Myaccount(){
         <div className={style.main}>
             <Header />
             <div className={style.body}>
-                
+                 <div className={style.top}>
+                    <h2>COMING SOON</h2>
+                 </div>
+                 <div className={style.middle}>
+                    <p>잠시만 기다려주세요.<br/>
+                    곧 찾아뵙겠습니다!</p>
+                 </div>
+                 <div className={style.bottom}>
+                    
+                 </div>
             </div>
         </div>
     )
